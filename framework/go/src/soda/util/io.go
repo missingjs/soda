59cd93696380148885818115fcae6c5b94706e0c
Tang Qiming <missingjs@outlook.com>
2021-11-17 20:28:13 +0800

    go: add two logger method

--- framework/go/src/soda/util/io.go
+++ framework/go/src/soda/util/io.go
@@ -11,8 +11,16 @@ func (g *SimpleLogger) Finfo(format string, a ...interface{}) {
     fmt.Fprintf(os.Stderr, "[INFO] " + format + "\n", a...)
 }
 
+func (g *SimpleLogger) Infof(format string, a ...interface{}) {
+    g.Finfo(format, a...)
+}
+
 func (g *SimpleLogger) Ferror(format string, a ...interface{}) {
     fmt.Fprintf(os.Stderr, "[ERROR] " + format + "\n", a...)
+}
+
+func (g *SimpleLogger) Errorf(format string, a ...interface{}) {
+    g.Ferror(format, a...)
 }
 
 func PrintErr(format string, a ...interface{}) (n int, err error) {
