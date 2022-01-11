dec97c231a3590cbea649aaf34cfa55d64cac088
Tang Qiming <missingjs@outlook.com>
2022-01-11 21:19:40 +0800

    refactor python validators

--- framework/python/src/soda/unittest/validate.py
+++ framework/python/src/soda/unittest/validate.py
@@ -2,7 +2,8 @@ from collections import defaultdict
 from abc import ABC, abstractmethod
 from typing import *
 
-class Descriptor(ABC):
+class ObjectFeature(ABC):
+
     @abstractmethod
     def hash(self, obj: Any) -> int:
         pass
@@ -17,13 +18,14 @@ class XEntry:
         self.value = value
 
 class XMap:
-    def __init__(self, descriptor: Descriptor):
-        self.desc = descriptor
+
+    def __init__(self, feature: ObjectFeature):
+        self.feat = feature
         self.dict = defaultdict(list)
         self.size = 0
 
     def _hash(self, key: Any) -> int:
-        return self.desc.hash(key)
+        return self.feat.hash(key)
 
     def __contains__(self, key: Any) -> bool:
         e = self._entry(key)
@@ -44,7 +46,7 @@ class XMap:
         if h not in self.dict:
             return
         for entry in self.dict[h]:
-            if self.desc.isEqual(key, entry.key):
+            if self.feat.isEqual(key, entry.key):
                 return entry
 
     def __setitem__(self, key: Any, val: Any):
@@ -62,89 +64,98 @@ class XMap:
             return
         e = None
         for entry in self.dict[h]:
-            if self.desc.isEqual(key, entry.key):
+            if self.feat.isEqual(key, entry.key):
                 e = entry
                 break
         if e is not None:
             self.dict[h].remove(e)
             self.size -= 1
 
-def unorderListComparator(desc: Descriptor):
-    def comp(a: List[Any], b: List[Any]) -> bool:
-        if len(a) != len(b):
-            return False
-        xmap = XMap(desc)
-        for e in a:
-            xmap[e] = xmap.get(e, 0) + 1
-        for e in b:
-            if e not in xmap:
-                return False
-            xmap[e] -= 1
-            if xmap[e] == 0:
-                del xmap[e]
-        return True
-    return comp
+class StrategyFactory:
 
-def listComparator(desc: Descriptor):
-    def comp(a: List[Any], b: List[Any]) -> bool:
-        if len(a) != len(b):
-            return False
-        for i in range(len(a)):
-            if not desc.isEqual(a[i], b[i]):
+    @classmethod
+    def unorderList(cls, feat: ObjectFeature):
+        def comp(a: List[Any], b: List[Any]) -> bool:
+            if len(a) != len(b):
                 return False
-        return True
-    return comp
+            xmap = XMap(feat)
+            for e in a:
+                xmap[e] = xmap.get(e, 0) + 1
+            for e in b:
+                if e not in xmap:
+                    return False
+                xmap[e] -= 1
+                if xmap[e] == 0:
+                    del xmap[e]
+            return True
+        return comp
 
-class DefaultDescriptor(Descriptor):
+    @classmethod
+    def list(cls, feat: ObjectFeature):
+        def comp(a: List[Any], b: List[Any]) -> bool:
+            if len(a) != len(b):
+                return False
+            for i in range(len(a)):
+                if not feat.isEqual(a[i], b[i]):
+                    return False
+            return True
+        return comp
+
+class GenericFeature(ObjectFeature):
+
     def hash(self, obj: Any) -> int:
         return hash(obj)
 
     def isEqual(self, a: Any, b: Any) -> bool:
         return a == b
 
-class ListDescriptor(Descriptor):
-    def __init__(self, elemDesc: Descriptor):
-        self.elemDesc = elemDesc
+class ListFeature(ObjectFeature):
+
+    def __init__(self, elemFeat: ObjectFeature):
+        self.elemFeat = elemFeat
 
     def hash(self, obj: List[Any]) -> int:
         res = 0
         # keep low 48 bits
         mask = 0xffffffffffff
         for e in obj:
-            h = self.elemDesc.hash(e)
+            h = self.elemFeat.hash(e)
             res = res * 133 + h
             res &= mask
         return res
 
     def isEqual(self, a: List[Any], b: List[Any]) -> bool:
-        return listComparator(self.elemDesc)(a, b)
+        return StrategyFactory.list(self.elemFeat)(a, b)
 
-class UnorderListDescriptor(Descriptor):
-    def __init__(self, elemDesc: Descriptor):
-        self.elemDesc = elemDesc
+class UnorderListFeature(ObjectFeature):
+
+    def __init__(self, elemFeat: ObjectFeature):
+        self.elemFeat = elemFeat
 
     def hash(self, obj: List[Any]) -> int:
         res = 0
         # keep low 48 bits
         mask = 0xffffffffffff
-        for h in sorted(map(lambda x: self.elemDesc.hash(x), obj)):
+        for h in sorted(map(lambda x: self.elemFeat.hash(x), obj)):
             res = res * 133 + h
             res &= mask
         return res
 
     def isEqual(self, a: List[Any], b: List[Any]) -> bool:
-        return unorderListComparator(self.elemDesc)(a, b)
+        return StrategyFactory.unorderList(self.elemFeat)(a, b)
 
 class Validators:
 
     @classmethod
     def forList(cls, ordered: bool):
-        d = DefaultDescriptor()
-        return listComparator(d) if ordered else unorderListComparator(d)
+        f = StrategyFactory
+        d = GenericFeature()
+        return f.list(d) if ordered else f.unorderList(d)
 
     @classmethod
     def forList2d(cls, dim1Ordered: bool, dim2Ordered: bool):
-        elemDesc = DefaultDescriptor()
-        d = ListDescriptor(elemDesc) if dim2Ordered else UnorderListDescriptor(elemDesc)
-        return listComparator(d) if dim1Ordered else unorderListComparator(d)
+        f = StrategyFactory
+        elemFeat = GenericFeature()
+        d = ListFeature(elemFeat) if dim2Ordered else UnorderListFeature(elemFeat)
+        return f.list(d) if dim1Ordered else f.unorderList(d)
 
