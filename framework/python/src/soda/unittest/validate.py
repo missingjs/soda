38ac456d78a5cc89bcb831807599e53c7ef603cb
Tang Qiming <missingjs@outlook.com>
2022-02-03 17:42:09 +0800

    python: class refactory

--- framework/python/src/soda/unittest/validate.py
+++ framework/python/src/soda/unittest/validate.py
@@ -10,11 +10,14 @@ class Validators:
 
     @classmethod
     def forList(cls, objType: type, ordered: bool) -> Callable[[Any,Any],bool]:
-        d = FeatureFactory.create(objType)
-        return ListFeature(d).isEqual if ordered else UnorderedListFeature(d).isEqual
+        return cls._for_list(ordered, FeatureFactory.create(objType))
 
     @classmethod
     def forList2d(cls, objType: type, dim1Ordered: bool, dim2Ordered: bool) -> Callable[[Any,Any],bool]:
         elemFeat = FeatureFactory.create(objType)
         d = ListFeature(elemFeat) if dim2Ordered else UnorderedListFeature(elemFeat)
-        return ListFeature(d).isEqual if dim1Ordered else UnorderedListFeature(d).isEqual
+        return cls._for_list(dim1Ordered, d)
+
+    @classmethod
+    def _for_list(cls, ordered: bool, elemFeat: 'ObjectFeature') -> Callable[[Any,Any],bool]:
+        return ListFeature(elemFeat).isEqual if ordered else UnorderedListFeature(elemFeat).isEqual
