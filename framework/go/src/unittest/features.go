5ace0b0993bcd9c4a2635b940d9f3dec3ec14670
Tang Qiming <missingjs@outlook.com>
2022-02-10 16:55:21 +0800

    go: refactor

--- framework/go/src/unittest/features.go
+++ framework/go/src/unittest/features.go
@@ -5,7 +5,6 @@ import (
     "hash/fnv"
     "math"
     "reflect"
-    "sort"
 )
 
 type ObjectFeature interface {
@@ -59,52 +58,6 @@ func (f ListFeature) IsEqual(a, b interface{}) bool {
         objb := vb.Index(i).Interface()
         if !f.ElemFeat.IsEqual(obja, objb) {
             return false
-        }
-    }
-    return true
-}
-
-type UnorderedListFeature struct {
-    ElemFeat ObjectFeature
-}
-
-func (f UnorderedListFeature) Hash(obj interface{}) uint64 {
-    vobj := reflect.ValueOf(obj)
-    hashes := make([]uint64, 0)
-    for i := 0; i < vobj.Len(); i++ {
-        hashes = append(hashes, f.ElemFeat.Hash(vobj.Index(i).Interface()))
-    }
-    sort.Slice(hashes, func(i, j int) bool { return hashes[i] < hashes[j] })
-    var res uint64
-    for _, h := range hashes {
-        res = res*133 + h
-    }
-    return res
-}
-
-func (f UnorderedListFeature) IsEqual(a, b interface{}) bool {
-    va, vb := reflect.ValueOf(a), reflect.ValueOf(b)
-    if va.Len() != vb.Len() {
-        return false
-    }
-    xmap := NewXMap(f.ElemFeat)
-    for i := 0; i < va.Len(); i++ {
-        key := va.Index(i).Interface()
-        c := xmap.GetOrDefault(key, 0).(int)
-        xmap.Put(key, c+1)
-    }
-
-
-    for i := 0; i < vb.Len(); i++ {
-        key := vb.Index(i).Interface()
-        if !xmap.Contains(key) {
-            return false
-        }
-        c := xmap.Get(key).(int)
-        if c > 1 {
-            xmap.Put(key, c-1)
-        } else {
-            xmap.Remove(key)
         }
     }
     return true
--- framework/go/src/unittest/unordered.go
+++ framework/go/src/unittest/unordered.go
@@ -1,5 +1,10 @@
 package unittest
 
+import (
+    "reflect"
+    "sort"
+)
+
 type XEntry struct {
     Key   interface{}
     Value interface{}
@@ -114,4 +119,49 @@ func (x *XMap) GetEntries() []*XEntry {
         res = append(res, v...)
     }
     return res
+}
+
+type UnorderedListFeature struct {
+    ElemFeat ObjectFeature
+}
+
+func (f UnorderedListFeature) Hash(obj interface{}) uint64 {
+    vobj := reflect.ValueOf(obj)
+    hashes := make([]uint64, 0)
+    for i := 0; i < vobj.Len(); i++ {
+        hashes = append(hashes, f.ElemFeat.Hash(vobj.Index(i).Interface()))
+    }
+    sort.Slice(hashes, func(i, j int) bool { return hashes[i] < hashes[j] })
+    var res uint64
+    for _, h := range hashes {
+        res = res*133 + h
+    }
+    return res
+}
+
+func (f UnorderedListFeature) IsEqual(a, b interface{}) bool {
+    va, vb := reflect.ValueOf(a), reflect.ValueOf(b)
+    if va.Len() != vb.Len() {
+        return false
+    }
+    xmap := NewXMap(f.ElemFeat)
+    for i := 0; i < va.Len(); i++ {
+        key := va.Index(i).Interface()
+        c := xmap.GetOrDefault(key, 0).(int)
+        xmap.Put(key, c+1)
+    }
+
+    for i := 0; i < vb.Len(); i++ {
+        key := vb.Index(i).Interface()
+        if !xmap.Contains(key) {
+            return false
+        }
+        c := xmap.Get(key).(int)
+        if c > 1 {
+            xmap.Put(key, c-1)
+        } else {
+            xmap.Remove(key)
+        }
+    }
+    return true
 }
