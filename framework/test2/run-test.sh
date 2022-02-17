7c757faed60f8bfba65ec02a0cc75dd19e029cb6
Tang Qiming <missingjs@outlook.com>
2022-02-17 12:46:11 +0800

    run: split location path

--- framework/test2/run-test.sh
+++ framework/test2/run-test.sh
@@ -5,7 +5,7 @@ usage()
     cat << EOF
 usage: $(basename $0) <location> <lang> [--clean]
 
-lang: all, cpp|python|java|go
+lang: all, cpp|python|java|go|scala|ruby|cs|perl
 EOF
     exit 1
 }
@@ -17,15 +17,21 @@ location=$1
 languages=$2
 shift; shift;
 [ -z "$location" -o -z "$languages" ] && usage
-[ "$languages" == "all" ] && languages="cpp python java go"
+[ "$languages" == "all" ] && languages="cpp python java go scala ruby cs perl"
 options="$@"
 
+for loc in $(echo $location | tr ':' ' '); do
+    echo location: $loc
+done
+
 for lang in $languages; do
-    while read path; do
-        directory=$(dirname $path)
-        cd $directory && echo "[$lang] Enter $directory"
-        soda run $lang $options || exit
-        cd .. && echo -e "[$lang] Leave $directory\n"
-    done < <(find $self_dir/$location -name $prj_file)
+    for loc in $(echo $location | tr ':' ' '); do
+        while read path; do
+            directory=$(dirname $path)
+            cd $directory && echo "[$lang] Enter $directory"
+            soda run $lang $options || exit
+            cd .. && echo -e "[$lang] Leave $directory\n"
+        done < <(find $self_dir/$loc -name $prj_file)
+    done
     echo -e "[$lang] DONE\n"
 done
