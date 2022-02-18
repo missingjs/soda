7b3d1163b4177ffcbe46a6341abae73fa20aa5ee
Tang Qiming <missingjs@outlook.com>
2022-02-18 21:34:54 +0800

    python: converter

--- framework/python/src/soda/unittest/registry.py
+++ framework/python/src/soda/unittest/registry.py
@@ -1,3 +1,4 @@
+import sys
 from typing import *
 
 from soda.leetcode.bitree import TreeNode, TreeFactory
@@ -11,10 +12,16 @@ converterFactoryMap = {}
 defaultConverter = ObjectConverter(lambda x: x, lambda y: y)
 
 def register(typeHint, funcFromSerial, funcToSerial):
-    converterFactoryMap[typeHint] = ObjectConverter(funcFromSerial, funcToSerial)
+    converterFactoryMap[typeHint] = lambda: ObjectConverter(funcFromSerial, funcToSerial)
+
+def registerFactory(typeHint, factoryFunc):
+    converterFactoryMap[typeHint] = factoryFunc
 
 def getConverter(typeHint):
-    return converterFactoryMap.get(typeHint, defaultConverter)
+    fact = converterFactoryMap.get(typeHint)
+    if fact:
+        return fact()
+    return defaultConverter
 
 register(ListNode, ListFactory.create, ListFactory.dump)
 register(Optional[ListNode], ListFactory.create, ListFactory.dump)
@@ -23,4 +30,4 @@ register(Optional[TreeNode], TreeFactory.create, TreeFactory.dump)
 register(NestedInteger, NestedIntegerFactory.parse, NestedIntegerFactory.serialize)
 register(List[NestedInteger], parseNestedIntegers, serializeNestedIntegers)
 register(List[ListNode], lambda L: list(map(ListFactory.create, L)), lambda L: list(map(ListFactory.dump, L)))
-
+registerFactory(List[Optional[ListNode]], lambda: getConverter(List[ListNode]))
