9038fad8536cfcfa143dca3e331b7f33e9f5f5fa
Qiming Tang <qiming.tang@tusimple.ai>
2022-02-23 17:53:56 +0800

    php: double feature

--- framework/php/src/Soda/Unittest/FeatureFactory.php
+++ framework/php/src/Soda/Unittest/FeatureFactory.php
@@ -9,6 +9,7 @@ class FeatureFactory
 
     static function create(string $type): ObjectFeature
     {
+        $c = count(self::$factoryMap);
         if (array_key_exists($type, self::$factoryMap)) {
             return self::$factoryMap[$type]();
         }
@@ -17,4 +18,32 @@ class FeatureFactory
             fn($x, $y) => $x === $y
         );
     }
+
+    private static function registerFactory($type, $hash, $equal)
+    {
+        self::regFact($type, fn() => new ObjectFeature($hash, $equal));
+    }
+
+    private static function regFact($type, $factory)
+    {
+        self::$factoryMap[$type] = $factory;
+    }
+
+    private static function regAlias($type, $alias)
+    {
+        self::regFact($alias, fn() => self::create($type));
+    }
+
+    static function __init()
+    {
+        self::registerFactory(
+            'double',
+            fn($obj) => Utils::hashCode($obj),
+            fn($x, $y) => abs($x-$y) < 1e-6
+        );
+        self::regAlias('double', 'Double');
+    }
 }
+
+FeatureFactory::__init();
+
