86323f9913f3ad16e7e15eefdf70c0952b4c0ac2
Qiming Tang <missingjs@outlook.com>
2022-03-01 11:57:45 +0800

    scala: optimize bootstrap file

--- framework/scala/src/main/scala/soda/scala/unittest/__Bootstrap__.scala
+++ framework/scala/src/main/scala/soda/scala/unittest/__Bootstrap__.scala
@@ -14,12 +14,20 @@ object Solution {
 
 class __Bootstrap__ extends (String => String) {
   override def apply(text: String): String = {
+    // * create work by method with N number of parameter
+    // val work = GenericTestWork.createN(Solution.METHOD)
+    // * OR, by method without return value and has only one parameter
+    // val work = GenericTestWork.create1u(Solution.METHOD_WITHOUT_RETURN)
+
     val work = GenericTestWork.create1(Solution.add)
+
     // val work = GenericTestWork.forStruct(typeOf[STRUCT])
     // val work = TestWork.forObject(typeOf[Solution], "METHOD")
     // val work = TestWork.forInstance(typeOf[Solution], "METHOD")
     // val work = TestWork.createN((...)=>R)
     // val work = TestWork.forStruct(typeOf[STRUCT])
+
+    // * setup validator
     // work.setValidator((R, R) => Boolean)
     work.compareSerial = true
     work.run(text)
