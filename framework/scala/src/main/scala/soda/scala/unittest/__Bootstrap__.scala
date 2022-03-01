85f705cd045cd7286c4aae9bfc9eec23e937ede3
Qiming Tang <missingjs@outlook.com>
2022-03-01 10:40:25 +0800

    scala: update bootstrap

--- framework/scala/src/main/scala/soda/scala/unittest/__Bootstrap__.scala
+++ framework/scala/src/main/scala/soda/scala/unittest/__Bootstrap__.scala
@@ -7,11 +7,14 @@ import soda.scala.leetcode._
 import collection.mutable
 import collection.mutable.{ArrayBuffer, ArrayDeque, PriorityQueue}
 import util.control.Breaks.{break, breakable}
-object Solution {}
+
+object Solution {
+  def add(a: Int): Int = a + 2004
+}
 
 class __Bootstrap__ extends (String => String) {
   override def apply(text: String): String = {
-    val work = GenericTestWork.create1(Solution.eq)
+    val work = GenericTestWork.create1(Solution.add)
     // val work = GenericTestWork.forStruct(typeOf[STRUCT])
     // val work = TestWork.forObject(typeOf[Solution], "METHOD")
     // val work = TestWork.forInstance(typeOf[Solution], "METHOD")
