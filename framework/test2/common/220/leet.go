bdb5f44e2bba12139345007737dc3da2afca01b0
Qiming Tang <missingjs@outlook.com>
2022-03-04 15:01:00 +0800

    go: drop unused code

--- framework/test2/common/220/leet.go
+++ framework/test2/common/220/leet.go
@@ -3,41 +3,12 @@ package main
 import (
     "fmt"
     "github.com/emirpasic/gods/maps/treemap"
-//    . "missingjs.com/soda/ds"
     . "missingjs.com/soda/leetcode"
     "missingjs.com/soda/unittest"
     "missingjs.com/soda/util"
 )
 
 var logger = util.Logger
-
-// func containsNearbyAlmostDuplicate(nums []int, k int, t int) bool {
-//     imap := NewSortedMap(func(i, j int)int { return i - j; })
-//     i, j := 0, 0
-//     for j < len(nums) {
-//         if j - i <= k {
-//             val := nums[j]
-//             j++
-//             lower := val - t
-//             upper := val + t
-//             key := imap.LowerBoundKey(lower)
-//             if key != nil && key.(int) <= upper {
-//                 return true
-//             }
-//             imap.Set(val, imap.GetOrDefault(val, 0).(int) + 1)
-//         } else {
-//             val := nums[i]
-//             i++
-//             c := imap.Get(val).(int) - 1
-//             if c == 0 {
-//                 imap.Del(val)
-//             } else {
-//                 imap.Set(val, c)
-//             }
-//         }
-//     }
-//     return false
-// }
 
 func containsNearbyAlmostDuplicate(nums []int, k int, t int) bool {
     imap := treemap.NewWithIntComparator()
