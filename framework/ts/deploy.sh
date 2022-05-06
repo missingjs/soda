2c51417257ccac6b795c162b794a58969712c2b1
Tang Qiming <missingjs@outlook.com>
2022-05-06 17:53:02 +0800

    ts: use docker for deployment

--- framework/ts/deploy.sh
+++ framework/ts/deploy.sh
@@ -8,7 +8,13 @@ cd $self_dir
 [ -d $out_dir ] && rm -r $out_dir
 
 set -x
-tsc --outDir $out_dir || exit
+docker run --rm -t \
+    --user $(id -u):$(id -g) \
+    -v "/etc/passwd:/etc/passwd:ro" \
+    -v "/etc/group:/etc/group:ro" \
+    -v $(pwd):/task \
+    -w /task \
+    missingjs/soda-ts tsc --outDir $out_dir
 set +x
 
 echo '{"type":"module"}' > dist/soda/package.json
