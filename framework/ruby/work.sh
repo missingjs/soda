286c71235d8b93e7258ee23938de781ab8995937
Tang Qiming <missingjs@outlook.com>
2022-05-12 12:26:28 +0800

    ruby: new docker procedure

--- framework/ruby/work.sh
+++ framework/ruby/work.sh
@@ -11,6 +11,9 @@ options:
     new <testname>
         create source file with name <testname>.rb
 
+    source <testname>
+        show source file name
+
     make <testname> 
         do nothing
 
@@ -34,15 +37,14 @@ cmd=$1
 testname=$2
 execfile=${testname}.rb
 
-assert_testname()
-{
-    [ -z $testname ] && usage
-}
-
+[ -z $testname ] && usage
 case $cmd in
     new)
         template_file=$self_dir/src/soda/unittest/bootstrap.rb
         create_source_file $template_file $execfile
+        ;;
+    source)
+        echo $execfile
         ;;
     make | clean)
         # Don't remove. Just for interface compatible
