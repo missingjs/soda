fa19317ae4aa35716b14e319012678e35d551664
Tang Qiming <missingjs@outlook.com>
2022-06-27 17:34:48 +0800

    scala: submit job by HTTP multipart

--- framework/scala/src/main/scala/soda/scala/web/work/WorkHandler.scala
+++ framework/scala/src/main/scala/soda/scala/web/work/WorkHandler.scala
@@ -5,31 +5,31 @@ import play.api.libs.json._
 import soda.scala.web.http.RequestHelper
 import soda.scala.web.resp.{Response, ResponseFactory}
 import soda.scala.web.bootstrap.ContextManager
-import soda.scala.web.{BaseHandler, Logger}
+import soda.scala.web.exception.{ParameterMissingException, ServiceException}
+import soda.scala.web.{BaseHandler, BusinessCode, Logger}
 
+import java.nio.charset.StandardCharsets
 import java.util.concurrent.{TimeUnit, TimeoutException}
 import scala.reflect.runtime.universe._
 
 class WorkHandler(private val contextManager: ContextManager, private val timeoutMillis: Long) extends BaseHandler {
 
   override def doPost(exchange: HttpExchange): Response = {
-    val content = RequestHelper.bodyString(exchange)
-    val jr = Json.parse(content).as[WorkRequest]
-
-    Logger.info(s"context key: ${jr.key}")
-    Logger.info(s"boot class: ${jr.bootClass}")
-    Logger.info(s"test case: ${jr.testCase}")
+    val req = WorkHandler.parse(exchange)
+    Logger.info(s"context key: ${req.key}")
+    Logger.info(s"boot class: ${req.entryClass}")
+    Logger.info(s"test case: ${req.testCase}")
 
     val task: () => String = () => {
       val oldCtxLoader = Thread.currentThread().getContextClassLoader
       try {
-        val classLoader = contextManager.get(jr.key) match {
+        val classLoader = contextManager.get(req.key) match {
           case Some(ctx) => ctx.getClassLoader
-          case None => throw new RuntimeException(s"no context found by key: ${jr.key}")
+          case None => throw new RuntimeException(s"no context found by key: ${req.key}")
         }
         Thread.currentThread().setContextClassLoader(classLoader)
         val rm = runtimeMirror(classLoader)
-        val csym = rm.staticClass(jr.bootClass)
+        val csym = rm.staticClass(req.entryClass)
         val classType = csym.typeSignature
         val cm = rm.reflectClass(csym)
         val ctor = cm.reflectConstructor(classType.decl(termNames.CONSTRUCTOR).asMethod)
@@ -37,7 +37,7 @@ class WorkHandler(private val contextManager: ContextManager, private val timeou
         val im = rm.reflect(obj)
         val applySym = classType.decl(TermName("apply"))
         val mm = im.reflectMethod(applySym.asMethod)
-        mm.apply(jr.testCase).asInstanceOf[String]
+        mm.apply(req.testCase).asInstanceOf[String]
       } finally {
         Thread.currentThread().setContextClassLoader(oldCtxLoader)
       }
@@ -53,6 +53,34 @@ class WorkHandler(private val contextManager: ContextManager, private val timeou
       case ex: TimeoutException =>
         job.kill()
         throw new RuntimeException("Work execution timeout", ex)
+    }
+  }
+}
+
+object WorkHandler {
+  private def parse(exchange: HttpExchange): WorkRequest = {
+    val contentType = exchange.getRequestHeaders.getFirst("Content-Type")
+    if (contentType.contains("application/json")) {
+      val content = RequestHelper.bodyString(exchange)
+      Json.parse(content).as[WorkRequest]
+    } else if (contentType.contains("multipart/form-data")) {
+      val formData = RequestHelper.multipartFormData(exchange)
+      val key = formData.firstValue("key") match {
+        case Some(key) => key
+        case None => throw new ParameterMissingException("key")
+      }
+      val entryClass = formData.firstValue("entry_class") match {
+        case Some(ec) => ec
+        case None => throw new ParameterMissingException("entry_class")
+      }
+      val caseBytes = formData.firstFile("test_case") match {
+        case Some(cb) => cb
+        case None => throw new ParameterMissingException("test_case")
+      }
+      val testCase = new String(caseBytes, StandardCharsets.UTF_8)
+      WorkRequest(key, entryClass, testCase)
+    } else {
+      throw new ServiceException(BusinessCode.COMMON_ERROR, "unknown Content-Type: " + contentType, 400)
     }
   }
 }
--- framework/scala/src/main/scala/soda/scala/web/work/WorkRequest.scala
+++ framework/scala/src/main/scala/soda/scala/web/work/WorkRequest.scala
@@ -2,7 +2,7 @@ package soda.scala.web.work
 
 import play.api.libs.json.{Json, OFormat}
 
-case class WorkRequest(key: String, bootClass: String, testCase: String)
+case class WorkRequest(key: String, entryClass: String, testCase: String)
 
 object WorkRequest {
   implicit val _format: OFormat[WorkRequest] = Json.format[WorkRequest]
