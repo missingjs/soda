2b79a3ad6dfb2f8991c9d2c5085d26eb294e3d39
Tang Qiming <missingjs@outlook.com>
2022-07-05 17:41:54 +0800

    drop unused code

--- framework/common/bashlib.sh
+++ framework/common/bashlib.sh
@@ -11,15 +11,6 @@ dku=$soda_dir/bin/docker-utils.sh
 
 export SODA_FRAMEWORK_DIR=$framework_dir
 
-command_run_help=\
-"    run <testname> [--testcase <files> [--delim <DELIM>]] [--verbose] [--timeout <SEC>]
-        run test case
-        
-        --testcase <files>    test case files, separated by --delim, default is 'test_data'
-        --delim <DELIM>       delimeter of file list, default ','
-        --timeout <SEC>       timeout in seconds, use decimal
-        --verbose             show test request & response"
-
 create_source_file()
 {
     local template_file=$1
@@ -27,13 +18,6 @@ create_source_file()
     [ -z $target_file ] && usage
     [ -e $target_file ] && { echo "Error: $target_file exists" >&2; exit 2; }
     cat $template_file > $target_file && echo "$target_file OK"
-}
-
-run_test()
-{
-    export PYTHONPATH="$framework_dir/python/src:$PYTHONPATH"
-    runner=soda.unittest.tester
-    python3 -m $runner "$@"
 }
 
 assert_testname()
